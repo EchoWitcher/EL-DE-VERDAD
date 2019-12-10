fbaa002c945aa2206928a15e82117caf8d468da8
EchoWitcher <drenrique.witcher@gmail.com>
2019-12-10 12:12:37 -0800

    EL proshecto
    Proyecto

---
+++ Proyecto/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+namespace Proyecto
+{
+   public class Producto
+    {
+        public string codigo 
+        {
+            get;
+            set;
+        }
+        public string descripcion
+        {
+            get;
+            set;
+        }
+        public Double precio
+        {
+            get;
+            set;
+        }
+        public Int16 departamento
+        {
+            get;
+            set;
+        }
+
+        public Int16 likes
+        {
+            get;
+            set;
+        }
+
+
+        public Producto(string code, string description, decimal price, int depto, int like)
+        {
+            codigo = code;
+            descripcion = description;
+            precio= price;
+            departamento =depto;
+            likes = like;
+        }
+
+    }
+    public class ProductoDB
+    {
+     
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+             Producto producto = new Producto();
+             try{
+                 //Paso 1
+            //Escribi la lista de los codigo
+            List<string> ListaCodigos = new List<string>() {("CONSOLECO","LAPPTOPP")};
+            //Escribi la lista de las Descripciones
+            List<string> ListaDescripcion = new List<string>() {"1. Negro, Incluye dos juegos", "2. Azul Marino, Incluye mouse" };
+            //Escribi la lista de los Precios
+            List<Decimal> ListaPrecio = new List<Decimal>() { 2500, 5000 };
+            //Escribi la lista del Departamento
+            List<int> ListaDepartamento = new List<int>() { 012, 080 };
+            //Escribi la lista del Likes
+            List<int> ListaLikes = new List<int>() { 1000, 589 };
+            using (StreamWriter EscribirDatos = new StreamWriter(@"D:\Proyecto_Producto\Datos.txt"))
+            {
+                //En el texto para que se vea estetico, ponemos que es cada lista
+                EscribirDatos.WriteLine("INFORMACION");
+                foreach (string c in ListaCodigos)
+                    {
+                    //Nuestro objeto Datos, que lleva la ruta, le asignamos la informacion de las listas a nuestro archivo Datos.txt
+                    //Y la "c" esta leyendo la lista de codigos que escribimos
+                        EscribirDatos.WriteLine(c.code);
+                        EscribirDatos.WriteLine(c.description);
+                        EscribirDatos.WriteLine(c.price);
+                        EscribirDatos.WriteLine(c.depto);
+                        EscribirDatos.WriteLine(c.like);
+                    }
+                //En el texto para que se vea estetico, ponemos que es cada lista
+                EscribirDatos.WriteLine("LISTA DE DESCRIPCION");
+                foreach (string d in ListaDescripcion)
+                {
+                    
+                   //Y la "d" esta leyendo la lista de descripciones que escribimos
+                    EscribirDatos.WriteLine(d);
+                }
+
+                //En el texto para que se vea estetico, ponemos que es cada lista
+                EscribirDatos.WriteLine("LISTA DE PRECIOS");
+                foreach (Decimal p in ListaPrecio)
+                {
+                    //Y la "p" esta leyendo la lista de precios que escribimos
+                    EscribirDatos.WriteLine(p);
+                }
+
+                //En el texto para que se vea estetico, ponemos que es cada lista
+                EscribirDatos.WriteLine("LISTA DEL DEPARTAMENTO");
+                foreach (int Dep in ListaDepartamento)
+                {
+                    //Y la "Dep" esta leyendo la lista de departamento que escribimos
+                    EscribirDatos.WriteLine(Dep);
+                }
+                //En el texto para que se vea estetico, ponemos que es cada lista
+                EscribirDatos.WriteLine("LISTA DEL LIKES");
+                foreach (int l in ListaLikes)
+                {
+                    //Y la "l" esta leyendo la lista de likes que escribimos
+                    EscribirDatos.WriteLine(l);
+                }
+            }
+            }
+            
+            catch
+            { }
+            
+            Console.ReadKey();
+        }
+    }
+}
+
+            
+   
---
+++ Proyecto2/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Proyecto2
+{
+    public class Producto
+    {
+        //Declaracion de las variables
+        public string codigo
+         { get; set; }
+        public string descripcion 
+        { get; set; }
+        public double precio 
+        { get; set; }
+        public int departamento 
+        { get; set; }
+        public int likes
+         { get; set; }
+
+//Asignamos el constructor para la asignacion de valores para cada objeto
+        
+        public Producto(string code, string description, double price, int depto, int Likes)
+        {
+             codigo = code; 
+             descripcion = description;
+             precio = price; 
+             departamento = depto; 
+             likes = Likes;
+        }
+
+        public enum FileType
+        {
+            Texto,
+            Binario
+        }
+        public  Producto()
+        {
+
+        }
+        
+    }
+    //Clase ProductoDB
+    public static class ProductoDB
+    {
+        //Usamos un contructor para nuestro archivo de texto, que usaremos para escribir los datos
+        //de los codigos, departamentos, precio, etc.
+        public static void EscribeProductosTXT(string archivo, List<Producto> Productos ,FileType FormatoT = FileType.Text )
+        {
+            switch (FormatoT)
+            {
+                case FileType.Text:
+                    EscribeTXT(archivo, productos);
+                    break;
+
+                case FileType.Binary:
+                    EscribeBIN(archivo, productos);
+                    break;
+            }
+//Accion para que vea el archivo, abrirlo y escribir en el
+            FileStream archivoFS = new FileStream(archivo , FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter txtOut = new StreamWriter(archivoFS);
+
+            foreach(Producto prod in Productos)
+            {
+                //Nuestro objeto Productos, que lleva la ruta, le asignamos la informacion de 
+                //las listas de Producto al archivo producto.txt
+             //Y la "prod" esta leyendo la lista de codigos que escribimos
+                txtOut.WriteLine(prod.codigo);
+                txtOut.WriteLine(prod.descripcion);
+                txtOut.WriteLine(prod.precio);
+                txtOut.WriteLine(prod.departamento);
+                txtOut.WriteLine(prod.likes);
+            }
+            //Para cerrar la lista de informacion
+            txtOut.Close();
+        }
+        //Leer producto TXT
+        public static List<Producto> LeeProductosTXT(string archivo)
+        {
+            //Lista para leer los producto que se encuetran en el archivo productos.txt
+        List<Producto> Productos = new List<Producto>();     
+        FileStream archivoFS = new FileStream(archivo , FileMode.Open, FileAccess.Read);   
+//Metodo que se utilia para leer los archivos
+        using( StreamReader txtIn = new StreamReader(archivoFS))
+        {
+            while( txtIn.Peek() != -1 )
+            {//Lectura de las variables de la clase Productos
+                Producto producto = new Producto();
+                producto.codigo = txtIn.ReadLine();
+                producto.descripcion = txtIn.ReadLine();
+                producto.precio = txtIn.Read();
+                producto.departamento = txtIn.Read();
+                producto.likes = txtIn.Read();
+                Productos.Add(producto);
+            }
+        }
+        return Productos;
+        }
+        //Escribimos el archivo en Binario
+        public static void EscribeProductosBIN(string archivo, List<Producto> Productos)
+        {
+            FileStream archivoFS= new FileStream(archivo, FileMode.OpenOrCreate, FileAccess.Write);
+            BinaryWriter binOut = new BinaryWriter(archivoFS);
+            foreach(Producto prod in Productos)
+            {
+                //Nuestro objeto Productos, que lleva la ruta, le asignamos la informacion de 
+                //las listas de Producto al archivo producto.txt, pero ahora a binario
+             //Y la "prod" esta leyendo la lista de codigos que escribimos
+                binOut.Write(prod.codigo);
+                binOut.Write(prod.descripcion);
+                binOut.Write(prod.precio);
+                binOut.Write(prod.departamento);
+                binOut.Write(prod.likes);
+            }
+            binOut.Close();
+        }
+        
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            
+            //Lista para agregar la informacion
+            List<Producto> Productos = new List<Producto>();
+            //Info va en orden(codigo, descripcion, precio, Departamento, Likes)
+            Productos.Add(new Producto("BASICOS"+"\n", "Guantes , Guias "+"\n", 13.00 , 3 , 130));
+
+            Productos.Add(new Producto("FUERA"+"\n", "Pala Picos , Rastrillos"+"\n", 50.200 , 3 , 543));
+
+            Productos.Add(new Producto("SEMILLA Y PLANTAS"+"\n", "Pasto Girasol Rosales"+"\n", 2.000, 4 , 21));
+            
+            Productos.Add(new Producto(" MACETA Y ABONO "+"\n" , "Maceta de Plastico Maceta de ceramica"+"\n", 37.000, 1 , 100));
+            //Transmite la informacion anterior a texto
+            ProductoDB.EscribeProductosTXT(@"Productos.txt", Productos);
+            //Transmite la informacion anterior a Binario
+            ProductoDB.EscribeProductosBIN(@"Productos.txt", Productos);
+
+
+            //Lecturas del archivo 
+            //Imprime de lectura TXT
+            List<Producto> productos_leidos = ProductoDB.LeeProductosTXT("Productos.txt");
+
+                foreach(Producto p in productos_leidos)
+                {    
+                    Console.WriteLine(p.codigo);
+                    Console.WriteLine(p.descripcion);
+                    Console.WriteLine(p.precio);
+                    Console.WriteLine(p.departamento);
+                    Console.WriteLine(p.likes);
+                }
+            
+
+        }
+    }
+}
